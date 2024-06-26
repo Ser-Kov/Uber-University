5f8ed55266260829d153acb55ebda955ac432562
Cергей Ковалевский <ser.kovalevskiy@gmail.com>
2024-06-26 12:01:47 +0300

    Домашнее задание по уроку "Специальные методы классов"

---
+++ Homework/Module 5/module_5_2.py
@@ -0,0 +1,10 @@
+class House:
+    def __init__(self):
+        self.numberOfFloors = 0
+
+    def setNewNumberOfFloors(self, floors):
+        self.numberOfFloors = floors
+        print(self.numberOfFloors)
+
+h1 = House
+h1.setNewNumberOfFloors(h1, 5)
