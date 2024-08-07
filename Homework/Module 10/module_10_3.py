f29059ab77399ff87b7c04510ac37f47bdd54312
Cергей Ковалевский <ser.kovalevskiy@gmail.com>
2024-08-07 21:33:35 +0300

    Задача "Банковские операции".

--- Homework/Module 10/module_10_3.py
+++ Homework/Module 10/module_10_3.py
@@ -1,5 +1,4 @@
-from threading import Lock
-import threading
+from threading import Lock, Thread
 from random import randint
 from time import sleep
 
@@ -12,29 +11,28 @@ class Bank:
     def deposit(self):
         for i in range(100):
             count = randint(50, 500)
-            self.balance += count
-            sleep(0.001)
-            print(f'Пополнение: {count}. Баланс: {self.balance}.')
-            if self.balance >= 500 and self.lock.locked():
-                self.lock.release()
+            with self.lock:
+                self.balance += count
+                sleep(0.001)
+                print(f'Пополнение: {count}. Баланс: {self.balance}.')
 
     def take(self):
         for i in range(100):
             count = randint(50, 500)
             print(f'Запрос на {count}')
-            if count <= self.balance:
-                self.balance -= count
-                sleep(0.001)
-                print(f'Снятие: {count}. Баланс: {self.balance}')
-            else:
-                print('Запрос отклонён, недостаточно средств')
-            self.lock.acquire()
+            with self.lock:
+                if count <= self.balance:
+                    self.balance -= count
+                    sleep(0.001)
+                    print(f'Снятие: {count}. Баланс: {self.balance}')
+                else:
+                    print('Запрос отклонён, недостаточно средств')
 
 
 bk = Bank()
 
-th1 = threading.Thread(target=Bank.deposit, args=(bk,))
-th2 = threading.Thread(target=Bank.take, args=(bk,))
+th1 = Thread(target=Bank.deposit, args=(bk,))
+th2 = Thread(target=Bank.take, args=(bk,))
 
 th1.start()
 th2.start()
