8559c5f36c56d7374c5bf70e26e3c2ba783d6b46
Cергей Ковалевский <ser.kovalevskiy@gmail.com>
2024-09-04 15:20:31 +0300

    Домашнее задание по теме "Сложные моменты и исключения в стеке вызовов функции"

--- Homework/Module 8/module_8_2.py
+++ Homework/Module 8/module_8_2.py
@@ -1,20 +1,29 @@
 def personal_sum(numbers):
     result = 0
+    incorrect_data = 0
+    correct_data = 0
     for i in numbers:
-        result += i
-    return result
+        try:
+            result += i
+            correct_data += 1
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
+    return (result, incorrect_data, correct_data)
 
 
 def calculate_average(numbers):
     try:
-        return personal_sum(numbers) / len(numbers)
+        sum_ = personal_sum(numbers)
+        result_avg = sum_[0] / sum_[2]
+        return result_avg
     except ZeroDivisionError:
         return 0
     except TypeError:
         return 'В numbers записан некорректный тип данных'
 
 
-print(f'Результат 1: {calculate_average("1, 2, 3")}')
-print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
-print(f'Результат 3: {calculate_average(567)}')
-print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
+print(f'Результат 1: {calculate_average("1, 2, 3")}') # Строка перебирается, но каждый символ - строковый тип
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}') # Учитываются только 1 и 3
+print(f'Результат 3: {calculate_average(567)}') # Передана не коллекция
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}') # Всё должно работать
